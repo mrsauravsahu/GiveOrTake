2df29f7c05a6537da19a943450e52531acc93bb2
Sahu, S <sauravsahu.12@live.com>
2017-05-15 18:10:27 +0530

    Move DataModels to correct namespace.

--- GiveOrTake.BackEnd/Data/RootAccess.cs
+++ GiveOrTake.BackEnd/Data/RootAccess.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace GiveOrTake.Database
+namespace GiveOrTake.BackEnd.Data
 {
     public partial class RootAccess
     {
--- GiveOrTake.BackEnd/Data/Transaction.cs
+++ GiveOrTake.BackEnd/Data/Transaction.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace GiveOrTake.Database
+namespace GiveOrTake.BackEnd.Data
 {
     public partial class Transaction
     {
