eaf82470ad05915802f03144415d11707bf4b65c
Sahu, S <sauravsahu.12@live.com>
2017-05-31 20:59:03 +0530

    Add Utilities Project to contain Extension Methods, an other reusable components.

---
+++ GiveOrTake.Utilities/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace GiveOrTake.Utilities
+{
+    public static class Extensions
+    {
+        public static string Capitalize(this string value)
+        {
+            if (value == string.Empty) return string.Empty;
+            return Regex.Replace(value, @"^[a-zA-Z]", m => m.Value.ToUpper());
+        }
+    }
+}
